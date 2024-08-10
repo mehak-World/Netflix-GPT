60b5bacfff999201fdda11bce91c42f2a5439503
mehak-World <139652649+mehak-World@users.noreply.github.com>
2024-08-09 17:50:22 -0700

    Initial commit. Started with the Netflix GPT project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className = "p-3 m-10">
+      <h1 className = "text-2xl text-blue-700">This is create react app</h1>
+    </div>
+  )
+}
+
+export default App
